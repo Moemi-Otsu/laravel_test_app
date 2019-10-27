bb10da23a860adf7eb11e37a29be3fa22040896a
moemi ohtsu <s0623192@gmail.com>
2019-10-27 15:16:30 +0900

    Add: artisanコマンドでauth機能を生成・migration実行

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,6 @@ Route::get('/', function () {
 Route::get('/', 'PostsController@index')->name('top');
 Route::resource('posts', 'PostsController', ['only' => ['create', 'store', 'show', 'edit', 'update', 'destroy']]);
 Route::resource('comments', 'CommentsController', ['only' => ['only', 'store']]);
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
