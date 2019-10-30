173e968f0a97e5c177b0eccc78f85aa7d248d97f
moemi ohtsu <s0623192@gmail.com>
2019-10-31 00:10:30 +0900

    Add: Userモデルにpost_id,comment_id追加、modelにアソシエーション追記"

---
+++ database/migrations/2019_10_27_180607_modify_users.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class ModifyUsers extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->integer('post_id')->unsigned();
+            $table->integer('comment_id')->unsigned();
+
+            $table->foreign('post_id')->references('id')->on('posts');
+            $table->foreign('comment_id')->references('id')->on('comments');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+        //
+        });
+    }
+}
